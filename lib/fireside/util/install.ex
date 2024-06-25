cdb22d89b10ac18c910db3edd2345f2b23ee8b13
Igor Barakaiev <ibarakaiev@gmail.com>
2024-06-26 00:45:16 +0300

    Add library installers

--- lib/fireside/util/install.ex
+++ lib/fireside/util/install.ex
@@ -2,7 +2,7 @@ defmodule Fireside.Util.Install do
   def install(component) do
     Mix.shell().info("Installing #{component}")
 
-    {component_name, [path: component_path]} = determine_dep_type_and_version(component)
+    {component_name, [path: component_path]} = determine_component_type_and_version(component)
 
     if not File.dir?(component_path) do
       raise "directory `#{component_path}` doesn't exist"
@@ -29,7 +29,6 @@ defmodule Fireside.Util.Install do
                 Path.join(component_path, glob)
                 |> GlobEx.compile!()
                 |> GlobEx.ls()
-                |> dbg()
 
               for file_path <- file_paths, reduce: %{igniter: igniter, lock: lock} do
                 %{igniter: igniter, lock: lock} ->
@@ -45,24 +44,137 @@ defmodule Fireside.Util.Install do
           end
       end
 
+    igniter = Igniter.create_new_elixir_file(igniter, "config/fireside.exs", transform_lock(lock))
+    {igniter, deps} = add_dependencies(igniter, component_path)
+
+    install_list = Enum.map(deps, &elem(&1, 0))
+
+    confirmation_message =
+      "Dependencies changes must go into effect before individual installers can be run. Proceed with changes?"
+
+    dependency_add_result =
+      Igniter.do_or_dry_run(igniter, [],
+        title: "Fetching Dependency",
+        quiet_on_no_changes?: true,
+        confirmation_message: confirmation_message
+      )
+
+    if dependency_add_result == :issues do
+      raise "Exiting due to issues found while fetching dependency"
+    end
+
+    if dependency_add_result == :dry_run_with_changes do
+      install_dep_now? =
+        Mix.shell().yes?("""
+        Cannot run any associated installers for the requested packages without
+        commiting changes and fetching dependencies.
+
+        Would you like to do so now? The remaining steps will be displayed as a dry run.
+        """)
+
+      if install_dep_now? do
+        Igniter.do_or_dry_run(igniter, ["--yes"],
+          title: "Fetching Dependency",
+          quiet_on_no_changes?: true
+        )
+      end
+    end
+
+    if dependency_add_result == :changes_aborted do
+      Mix.shell().info("\nChanges aborted by user request.")
+    else
+      Mix.shell().info("running mix deps.get")
+
+      case Mix.shell().cmd("mix deps.get") do
+        0 ->
+          Mix.Project.clear_deps_cache()
+          Mix.Project.pop()
+
+          "mix.exs"
+          |> File.read!()
+          |> Code.eval_string([], file: Path.expand("mix.exs"))
+
+          Mix.Dep.clear_cached()
+          Mix.Project.clear_deps_cache()
+
+          Mix.Task.run("deps.compile")
+
+          Mix.Task.reenable("compile")
+          Mix.Task.run("compile")
+
+        exit_code ->
+          Mix.shell().info("""
+          mix deps.get returned exited with code: `#{exit_code}`
+          """)
+      end
+
+      igniter =
+        Igniter.new()
+        |> Igniter.assign(%{manually_installed: install_list})
+
+      desired_tasks = Enum.map(install_list, &"#{&1}.install")
+
+      Mix.Task.load_all()
+      |> Stream.map(fn item ->
+        Code.ensure_compiled!(item)
+        item
+      end)
+      |> Stream.filter(&Igniter.Util.Install.implements_behaviour?(&1, Igniter.Mix.Task))
+      |> Stream.filter(&(Mix.Task.task_name(&1) in desired_tasks))
+      |> Enum.reduce(igniter, fn task, igniter ->
+        Igniter.compose_task(igniter, task, [])
+      end)
+      |> Igniter.do_or_dry_run([])
+    end
+
+    :ok
+  end
+
+  defp transform_lock(lock) do
     hash =
       :crypto.hash(:sha, Enum.sort(lock) |> Enum.map(& &1.hash) |> Enum.join(""))
       |> Base.encode16()
 
     # TODO: or update
-    lockfile =
-      quote do
-        config :fireside,
-          shopifex: %{
-            hash: unquote(hash),
-            files: unquote(lock)
-          }
-      end
-      |> Macro.to_string()
+    quote do
+      %{
+        shopifex: %{
+          hash: unquote(hash),
+          files: unquote(lock)
+        }
+      }
+    end
+    |> Macro.to_string()
+  end
 
-    igniter = Igniter.create_new_elixir_file(igniter, "config/fireside.exs", lockfile)
+  def add_dependencies(igniter, component_path) do
+    mix_file = Path.join(component_path, "mix.exs")
 
-    Igniter.do_or_dry_run(igniter, [])
+    unless File.exists?(mix_file) do
+      raise "mix.exs not found in the component directory"
+    end
+
+    {deps, _} =
+      mix_file
+      |> File.read!()
+      |> Sourceror.parse_string!()
+      |> Sourceror.Zipper.zip()
+      |> Igniter.Code.Module.move_to_defp(:deps, 0)
+      |> then(fn {:ok, zipper} ->
+        zipper
+      end)
+      |> Sourceror.Zipper.node()
+      |> Code.eval_quoted()
+
+    {Enum.reduce(deps, igniter, fn dep, igniter ->
+       {name, version, opts} =
+         case dep do
+           {name, version, opts} = dep -> dep
+           {name, version} -> {name, version, []}
+         end
+
+       Igniter.Project.Deps.add_dependency(igniter, name, version, opts)
+     end), deps}
   end
 
   defp prepare_ast_with_hash(file_path, component_name) do
@@ -139,12 +251,12 @@ defmodule Fireside.Util.Install do
     Module.concat(module_name)
   end
 
-  defp determine_dep_type_and_version(requirement) do
+  defp determine_component_type_and_version(requirement) do
     case String.split(requirement, "@", trim: true) do
-      [_package] ->
+      [_component] ->
         raise "only @path components are currently supported"
 
-      [package, version] ->
+      [component, version] ->
         case version do
           "git:" <> _requirement ->
             raise "@git components are not yet supported"
@@ -163,7 +275,7 @@ defmodule Fireside.Util.Install do
             :error
 
           requirement ->
-            {package, requirement}
+            {component, requirement}
         end
     end
   end
--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Fireside.MixProject do
 
   defp deps do
     [
-      {:igniter, path: "../igniter"}
+      {:igniter, path: "../igniter", override: true}
     ]
   end
 end
